f7cc2c8ab90bf829472e46055e0f3c90fd2b9fba
lukaszkobylecki <lukaszkobylecki@wp.pl>
2018-10-10 00:27:02 +0200

    Merge branch '06-game-manager'

---
+++ Assets/Scripts/StageManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    public GameObject TimeManagerObject;
+
+    public StageType StageType { get { return _stageType; } }
+
+    private StageType _stageType;
+    private TimeManager _timeManager;
+
+	void Start()
+    {
+        Initialize();
+	}
+
+    public void ChangeStage(StageType stageType)
+    {
+        if (stageType == StageType.Game)
+            ChangeStageToGame();
+        else if (stageType == StageType.Forge)
+            ChangeStageToForge();
+
+        _stageType = stageType;
+    }  
+
+    public void ChangeStageToGame()
+    {
+        _timeManager.TimeStart();
+    }
+
+    public void ChangeStageToForge()
+    {
+        _timeManager.TimeStop();
+    }
+
+    private void Initialize()
+    {
+        _stageType = StageType.Game;
+        _timeManager = TimeManagerObject.GetComponent<TimeManager>();
+    }
+}
