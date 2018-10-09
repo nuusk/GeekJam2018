5c05c2742906f22c898d5d482416638103c01c4b
lukaszkobylecki <lukaszkobylecki@wp.pl>
2018-10-09 22:07:00 +0200

    time control
    - add game manager script
    - count down time
    - display time
    - time factor (to slow down or speed up)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
+    public GameObject TextManager;
+
+    [Space]
+    public int TotalTime = 100;
+
+    public int TimeLeft { get { return (int)Math.Truncate(_timeLeft); } }
+    public bool TimeStopped { get { return _timeStopped; } }
+    public bool GameEnded { get { return _gameEnded; } }
+    public float TimeFactor { get { return _timeFactor; } }
+
+    private float _timeLeft;
+    private bool _timeStopped;
+    private bool _gameEnded;
+    private float _timeFactor;
+
+    void Awake()
+    {
+        SetGameManager();
+    }
+
+	void Start()
+    {
+		Initialize();
+	}
+	
+	void Update()
+    {
+        UpdateTime();
+	}
+
+    private void SetGameManager()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+            Destroy(this.gameObject);
+    }
+
+    private void Initialize()
+    {
+        _timeLeft = TotalTime;
+        _timeStopped = false;
+        _gameEnded = false;
+        _timeFactor = 1.0f;
+    }
+
+    private void UpdateTime()
+    {
+        if (!CanUpdateTime())
+            return;
+
+        _timeLeft -= Time.deltaTime * _timeFactor;
+
+        if (_timeLeft <= 0)
+        {
+            _timeLeft = 0;
+            GameEnd();
+        }
+    }
+
+    private bool CanUpdateTime()
+    {
+        return (!_gameEnded && !_timeStopped);
+    }
+    
+    private void GameEnd()
+    {
+        _gameEnded = true;
+    }
+}
