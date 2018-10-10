fe9c9b85492e71c82b5451a21bf8090de28fd881
poe <piotr.a.ptak@icloud.com>
2018-10-11 00:47:41 +0200

    Add billboard prefabs and resolve parallax errors

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -19,10 +19,12 @@ public class Parallax : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		cameraTransform = Camera.main.transform;
+		
 		lastCameraX = cameraTransform.position.x;
 		lastCameraY = cameraTransform.position.y;
 		layers = new Transform[transform.childCount];
 		Debug.Log(layers);
+		Debug.Log(layers.Length);
 		for (int i=0; i<transform.childCount; i++) {
 			layers[i] = transform.GetChild(i);
 		}
@@ -32,16 +34,18 @@ public class Parallax : MonoBehaviour {
 	}
 
 	private void ScrollLeft() {
+		Debug.Log("Scrolling Left");
 		int lastRight = rightIndex;
 		layers[rightIndex].position = Vector3.right * (layers[leftIndex].position.x - backgroundSize);
 		leftIndex = rightIndex;
 		rightIndex++;
-		if(rightIndex < 0) {
-			rightIndex = layers.Length-1;
+		if(rightIndex > layers.Length - 1) {
+			rightIndex = 0;
 		}
 	}
 
 	private void ScrollRight() {
+		Debug.Log("Scrolling Right");
 		int lastLeft = leftIndex;
 		layers[leftIndex].position = Vector3.right * (layers[rightIndex].position.x + backgroundSize);
 		rightIndex = leftIndex;
@@ -55,16 +59,19 @@ public class Parallax : MonoBehaviour {
 	void Update () {
 		float deltaX = cameraTransform.position.x - lastCameraX;
 		float deltaY = cameraTransform.position.y - lastCameraY;
+		Debug.Log(cameraTransform.position.x);
 		transform.position += Vector3.right * (deltaX * parallaxSpeedX);
 		transform.position += Vector3.up * (deltaY * parallaxSpeedY);
+
 		lastCameraX = cameraTransform.position.x;
 		lastCameraY = cameraTransform.position.y;
+		Debug.Log(rightIndex);
 
 		if (cameraTransform.position.x < (layers[leftIndex].transform.position.x + viewZone)) {
 			ScrollLeft();
 		}
 
-		if (cameraTransform.position.x > (layers[leftIndex].transform.position.x - viewZone)) {
+		if (cameraTransform.position.x > (layers[rightIndex].transform.position.x - viewZone)) {
 			ScrollRight();
 		}
 	}
