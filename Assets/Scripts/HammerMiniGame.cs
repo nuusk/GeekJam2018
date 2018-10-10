1028315af4996e958ff8a1f4e208ee66a97c1c8a
lukaszkobylecki <lukaszkobylecki@wp.pl>
2018-10-10 17:52:55 +0200

    add hammer minigame

---
+++ Assets/Scripts/HammerMiniGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerMiniGame : MonoBehaviour
+{
+    public float RoundTime = 10f;
+    public float TimeLeft;
+
+    void Start()
+    {
+        TimeLeft = RoundTime;
+    }
+
+    void Update()
+    {
+        UpdateTime();
+
+        if (TimeLeft <= 0)
+        {
+            TimeLeft = 0;
+            Lose();
+        }
+    }
+
+    public void Lose()
+    {
+        Debug.Log("Przegrałeś");
+    }
+
+    public void Win()
+    {
+        Debug.Log("Wygrałeś");
+    }
+
+    private void UpdateTime()
+    {
+        TimeLeft -= Time.deltaTime;
+    }
+}
