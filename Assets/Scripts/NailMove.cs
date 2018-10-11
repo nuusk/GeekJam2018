2b3ebef1b16d9949bb5ca0f8a8ea31fd78405a0b
lukaszkobylecki <lukaszkobylecki@wp.pl>
2018-10-11 13:54:49 +0200

    last one :fire:

--- Assets/Scripts/NailMove.cs
+++ Assets/Scripts/NailMove.cs
@@ -4,28 +4,21 @@ using UnityEngine;
 
 public class NailMove : MonoBehaviour
 {
-    public double amplitude;
-    public double frequency;
-    private int direction;
+    private float amplitude;
+    private float frequency;
 
     // Use this for initialization
     void Start()
     {
-        amplitude = UnityEngine.Random.Range(0, 0.3f);
-        frequency = UnityEngine.Random.Range(0, 0.1f);
-        direction = UnityEngine.Random.Range(0, 2) == 0 ? 1 : -1;
-        this.transform.localScale = new Vector3(
-            direction,
-            this.transform.localScale.y,
-            this.transform.localScale.z
-            );
+        amplitude = UnityEngine.Random.Range(0, 0.5f);
+        frequency = UnityEngine.Random.Range(0, 1f);
     }
 
     void Update()
     {
         this.transform.position = new Vector3(
             this.transform.position.x,
-            this.transform.position.y + (float)amplitude * Mathf.Sin((float)frequency * Time.frameCount),
+            this.transform.position.y + amplitude * Mathf.Sin(frequency * Time.frameCount),
             this.transform.position.z
             );
     }
